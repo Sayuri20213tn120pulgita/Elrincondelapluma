e181a2c3168f310a1b3d1d508c972cc045e1f4ea
Sayuri20213tn120pulgita <20213tn120@utez.edu.mx>
2024-12-05 01:54:46 -0600

    Cambio base

--- config.py
+++ config.py
@@ -5,16 +5,16 @@ class Config:
 
 class DevelopmentConfig (Config):
     DEBUG = True
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''
+    MYSQL_HOST = 'elrincon.clo6mk4aoxoh.us-east-1.rds.amazonaws.com'
+    MYSQL_USER = 'admin'
+    MYSQL_PASSWORD = '20213tn120GS#'
     MYSQL_DB = 'tienda'
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587 # TLS: TRANSPORT LAYER SECURITY: SEGURIDAD DE LA CAPA DE TRANSPORTE
     MAIL_USE_TLS = True
     MAIL_USERNAME = '20213tn120@utez.edu.mx'
     MAIL_PASSWORD = config('MAIL_PASSWORD')
-print(f"fdbgfdbghjjfghjkfdhgjkfdhkgjdfhgjfhhjhhjhgdjhkjgrdhgdkjfbversuhvtrubtyrbgf¨{config('MAIL_PASSWORD')}")
+
 config = {
     'development': DevelopmentConfig,
     'default': DevelopmentConfig
